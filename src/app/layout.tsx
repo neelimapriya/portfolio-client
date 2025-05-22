f2add20d239061204a9e487b5e16c61404a1870c
Nelima sultana <neelimasultana6@gmail.com>
2025-05-22 22:41:35 +0600

    add skills

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,10 @@ import "./globals.css";
 import Providers from "@/lib/providers";
 import Link from "next/link";
 
-
-
 export const metadata: Metadata = {
   title: "Nelima's Portfolio",
-  description:     "Nelima Sultana is a full-stack developer passionate about building scalable, high-performance web applications. She has hands-on experience developing full-stack solutions using a variety of modern technologies.",
-
+  description:
+    "Nelima Sultana is a full-stack developer passionate about building scalable, high-performance web applications. She has hands-on experience developing full-stack solutions using a variety of modern technologies.",
 };
 
 export default function RootLayout({
@@ -17,29 +15,42 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <html lang="en" suppressHydrationWarning>
-    <body className="bg-black text-white font-sans">
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-black text-white font-sans">
         <div className="flex min-h-screen">
-          <aside className="w-20 md:w-40 bg-black text-white flex flex-col items-center py-10 space-y-6 border-r border-gray-800">
+          {/* Sidebar */}
+          <aside className="sticky top-0 z-50 w-20 md:w-40 bg-black text-white flex flex-col items-center py-10 space-y-6 border-r border-gray-800">
             <NavItem icon="🏠" label="Home" href="/" />
             <NavItem icon="👩‍💻" label="About" href="/about" />
-            <NavItem icon="📚 " label="Skills" href="/skills" />
+            <NavItem icon="📚" label="Skills" href="/skills" />
             <NavItem icon="📁" label="Projects" href="/projects" />
             <NavItem icon="✉️" label="Contact" href="/contact" />
           </aside>
-          <main className="flex-1 p-10"><Providers>{children}</Providers></main>
+
+          {/* Main content */}
+          <main className="flex-1 overflow-y-auto p-6 md:p-10">
+            <Providers>{children}</Providers>
+          </main>
         </div>
       </body>
     </html>
   );
 }
-function NavItem({ icon, label, href }: { icon: string; label: string; href: string }) {
+function NavItem({
+  icon,
+  label,
+  href,
+}: {
+  icon: string;
+  label: string;
+  href: string;
+}) {
   return (
-    <Link href={href} style={{ textShadow: '0 0 20px  #fff' }}>
-      <div  className="flex  items-center text-sm space-x-1 space-y-1 hover:text-[#F4FBA3] cursor-pointer">
+    <Link href={href} style={{ textShadow: "0 0 20px  #fff" }}>
+      <div className="flex  items-center text-sm space-x-1 space-y-1 hover:text-[#F4FBA3] cursor-pointer">
         <span className="text-2xl">{icon}</span>
         <span>{label}</span>
       </div>
     </Link>
-  )
+  );
 }
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,175 @@
-import React from 'react';
+
+
+type skills = {
+  skill: string;
+  level: string;
+};
+
+const frontEndSkillData: skills[] = [
+  {
+    skill: "Html",
+    level: "Expert",
+  },
+  {
+    skill: "CSS",
+    level: "Expert",
+  },
+  {
+    skill: "JavaScript",
+    level: "Expert",
+  },
+  {
+    skill: "Tailwind CSS",
+    level: "Expert",
+  },
+  {
+    skill: "ReactJs",
+    level: "Expert",
+  },
+  {
+    skill: "NextJs",
+    level: "Expert",
+  },
+  {
+    skill: "Material UI",
+    level: "Expert",
+  },
+  {
+    skill: "Shadcn UI",
+    level: "Expert",
+  },
+  {
+    skill: "Typescript",
+    level: "Expert",
+  },
+  {
+    skill: "Redux",
+    level: "Intermediate",
+  },
+];
+const backendEndSkillData: skills[] = [
+  {
+    skill: "Nodejs",
+    level: "Intermediate",
+  },
+  {
+    skill: "Express",
+    level: "Expert",
+  },
+  {
+    skill: "MongoDB",
+    level: "Expert",
+  },
+  {
+    skill: "Mongoose",
+    level: "Expert",
+  },
+  {
+    skill: "JWT",
+    level: "Expert",
+  },
+  {
+    skill: "Cookies",
+    level: "Expert",
+  },
+  {
+    skill: "DBMS",
+    level: "Intermediate",
+  },
+  {
+    skill: "SQL",
+    level: "Intermediate",
+  },
+  {
+    skill: "PostgreSQL",
+    level: "Intermediate",
+  },
+  {
+    skill: "Prisma",
+    level: "Intermediate",
+  },
+];
 
 const SkillsPage = () => {
-    return (
-        <div>
-            <h2>Skills</h2>
+  return (
+    <div className="">
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="overflow-x-auto bg-black text-white p-4 rounded-lg md:w-1/2">
+          <h2 className="text-lg font-semibold mb-4">FrontEnd Skills</h2>
+          <table className="w-full border border-gray-700 text-sm">
+            <thead>
+              <tr className="bg-[#F4FBA3] text-black">
+                <th className="border border-gray-700 px-4 py-2 text-left">
+                  Skills
+                </th>
+                <th className="border border-gray-700 px-4 py-2 text-left">
+                  Level
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {frontEndSkillData.map((data, index) => (
+                <tr key={index}>
+                  <td className="border border-gray-700 px-4 py-2">
+                    {data.skill}
+                  </td>
+                  <td className="border border-gray-700 px-4 py-2">
+                    {data.level}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+        <div className="overflow-x-auto bg-black text-white p-4 rounded-lg md:w-1/2">
+          <h2 className="text-lg font-semibold mb-4">BackEnd Skills</h2>
+          <table className="w-full border border-gray-700 text-sm">
+            <thead>
+              <tr className="bg-[#F4FBA3] text-black">
+                <th className="border border-gray-700 px-4 py-2 text-left">
+                  Skills
+                </th>
+                <th className="border border-gray-700 px-4 py-2 text-left">
+                  Level
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {backendEndSkillData.map((data, index) => (
+                <tr key={index}>
+                  <td className="border border-gray-700 px-4 py-2">
+                    {data.skill}
+                  </td>
+                  <td className="border border-gray-700 px-4 py-2">
+                    {data.level}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="overflow-x-auto bg-black text-white p-4 rounded-lg w-full md:w-1/2">
+        <h2 className="text-lg font-semibold mb-4">Other Skills</h2>
+        <table className="w-full border border-gray-700 text-sm">
+          <tbody>
+            <tr >
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Github</td>
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Wordpress</td>
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Figma</td>
+            
+            </tr>
+            <tr >
+            
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Postman</td>
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Vercel</td>
+              <td className="border border-gray-700 px-4 py-2 text-[#F4FBA3]">Netlify</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default SkillsPage;
