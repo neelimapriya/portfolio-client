bb6069c35014f9f4f4d404ca52beb65a960da8f5
Nelima sultana <neelimasultana6@gmail.com>
2025-05-27 20:39:46 +0100

    add light mode to about page

--- src/app/(portfolio)/about/page.tsx
+++ src/app/(portfolio)/about/page.tsx
@@ -1,93 +1,69 @@
-import Link from "next/link";
-import React from "react";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import Link from 'next/link';
+import React from 'react';
 
 const AboutPage = () => {
   return (
     <div>
       <div>
-        <h2 className="">Hi, I am <span className="text-xl font-bold">Nelima Sultana</span></h2>
+        <h2 className="">
+          Hi, I am <span className="font-medium">Nelima Sultana</span>
+        </h2>
         <br />
-        <p className=" w-3/4">
-          As a <span className="text-primary"> Full Stack Web Developer</span>
-          , I design and develop dynamic, responsive, and user-friendly web
-          applications from end to end. My work spans across crafting intuitive
-          front-end interfaces using <span className="text-primary">HTML, CSS, JavaScript</span>, and modern
-          frameworks like <span className="text-primary">React, NextJs</span>, to building scalable back-end systems with <span className="text-primary">Node.js and Express</span>.
-           I’m experienced in working with both relational
-          and non-relational databases such as <span className="text-primary">MySQL and MongoDB</span>, and I
-          regularly create and integrate RESTful APIs. I use Git for version
-          control, manage deployments with platforms like Vercel or Netlify, and
-          ensure optimized performance across devices. With a focus on clean
-          code, scalability, and great user experience, I’m passionate about
-          turning complex problems into simple, efficient solutions.
+        <p className="max-w-xl">
+          As a <span className="text-primary font-medium"> Full Stack Web Developer</span>, I design and develop dynamic, responsive, and user-friendly web applications from end to end. My work spans
+          across crafting intuitive front-end interfaces using <span className="text-primary font-medium">HTML, CSS, JavaScript</span>, and modern frameworks like{' '}
+          <span className="text-primary font-medium">React, NextJs</span>, to building scalable back-end systems with <span className="text-primary font-medium">Node.js and Express</span>. I’m
+          experienced in working with both relational and non-relational databases such as <span className="text-primary font-medium">MySQL and MongoDB</span>, and I regularly create and integrate
+          RESTful APIs. I use Git for version control, manage deployments with platforms like Vercel or Netlify, and ensure optimized performance across devices. With a focus on clean code,
+          scalability, and great user experience, I’m passionate about turning complex problems into simple, efficient solutions.
         </p>
-        
       </div>
-      <div className="overflow-x-auto py-4 rounded-lg">
-        <h2 className="text-lg font-semibold mb-4">Education</h2>
-        <table className="w-full border border-gray-700 text-sm">
-          <thead>
-            <tr className="bg-[#F4FBA3] text-black">
-              <th className="border border-gray-700 px-4 py-2 text-left">
-                Degree
-              </th>
-              <th className="border border-gray-700 px-4 py-2 text-left">
-                Institution
-              </th>
-              <th className="border border-gray-700 px-4 py-2 text-left">
-                Year
-              </th>
-              <th className="border border-gray-700 px-4 py-2 text-left">
-                Certificate
-              </th>
-            </tr>
-          </thead>
-          <tbody>
+      <div className="overflow-x-auto py-4 rounded-lg mt-2">
+        <h2 className="text-lg font-semibold mb-2">Education</h2>
+        <Table className="w-full text-sm shadow-2xl">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Degree</TableHead>
+              <TableHead>Institution</TableHead>
+              <TableHead>Year</TableHead>
+              <TableHead>Certificate</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
             {/* 1 */}
-            <tr>
-              <td className="border border-gray-700 px-4 py-2">
-                Next Level Web Development 2.0 [Batch-4]
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                Programming Hero
-              </td>
-              <td className="border border-gray-700 px-4 py-2">2025</td>
-              <td className="border border-gray-700 px-4 py-2"> <Link style={{ textShadow: '0 0 20px  #F4FBA3' }} href="https://drive.google.com/file/d/1pysCVgCPRjfw9lMNKalA4f3R2DxtQxeX/view?usp=sharing">Click🔗</Link>  </td>
-            </tr>
+            <TableRow>
+              <TableCell className="font-medium text-primary">Next Level Web Development 2.0 [Batch-4]</TableCell>
+              <TableCell>Programming Hero</TableCell>
+              <TableCell>2025</TableCell>
+              <TableCell>
+                {' '}
+                <Link style={{ textShadow: '0 0 20px  #F4FBA3' }} href="https://drive.google.com/file/d/1pysCVgCPRjfw9lMNKalA4f3R2DxtQxeX/view?usp=sharing">
+                  Click🔗
+                </Link>{' '}
+              </TableCell>
+            </TableRow>
             {/* 2 */}
-            <tr>
-              <td className="border border-gray-700 px-4 py-2">
-                Complete Web Development Course [Batch-8]
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                Programming Hero
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-               2023
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                <Link style={{ textShadow: '0 0 20px  #F4FBA3' }} href="https://drive.google.com/file/d/1fqcI48v-kt7o47nTRu4oyOdXBx-HBIua/view?usp=sharing"> Click🔗</Link>
-               
-              </td>
-            </tr>
+            <TableRow>
+              <TableCell className="font-medium text-primary">Complete Web Development Course [Batch-8]</TableCell>
+              <TableCell>Programming Hero</TableCell>
+              <TableCell>2023</TableCell>
+              <TableCell>
+                <Link style={{ textShadow: '0 0 20px  #F4FBA3' }} href="https://drive.google.com/file/d/1fqcI48v-kt7o47nTRu4oyOdXBx-HBIua/view?usp=sharing">
+                  {' '}
+                  Click🔗
+                </Link>
+              </TableCell>
+            </TableRow>
             {/* 3 */}
-            <tr>
-              <td className="border border-gray-700 px-4 py-2">
-                Bachelor of Science in Zoology
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                National University
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                2018 – 2024
-              </td>
-              <td className="border border-gray-700 px-4 py-2">
-                {/* <Link style={{ textShadow: '0 0 20px  #fff' }} href="https://drive.google.com/file/d/1yckCCFWwSq0UOWd2U_vR2JUkpKsX8RTq/view?usp=sharing">Click🔗</Link> */}
-                
-              </td>
-            </tr>
-          </tbody>
-        </table>
+            <TableRow>
+              <TableCell className="font-medium text-primary">Bachelor of Science in Zoology</TableCell>
+              <TableCell>National University</TableCell>
+              <TableCell>2018 – 2024</TableCell>
+              <TableCell>{/* <Link style={{ textShadow: '0 0 20px  #fff' }} href="https://drive.google.com/file/d/1yckCCFWwSq0UOWd2U_vR2JUkpKsX8RTq/view?usp=sharing">Click🔗</Link> */}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
