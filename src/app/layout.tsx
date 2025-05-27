007c5f24fb90167d11c4ed80af3aeddcd0825210
Nelima sultana <neelimasultana6@gmail.com>
2025-05-27 08:36:37 +0100

    group routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Providers from "@/lib/providers";
-import Link from "next/link";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: "Nelima's Portfolio",
   description:
-    "Nelima Sultana is a full-stack developer passionate about building scalable, high-performance web applications. She has hands-on experience developing full-stack solutions using a variety of modern technologies.",
+    'Nelima Sultana is a full-stack developer passionate about building scalable, high-performance web applications. She has hands-on experience developing full-stack solutions using a variety of modern technologies.',
 };
 
 export default function RootLayout({
@@ -16,41 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-black text-white font-sans">
-        <div className="flex min-h-screen">
-          {/* Sidebar */}
-          <aside className="sticky top-0 z-50 w-20 md:w-40 bg-black text-white flex flex-col items-center py-10 space-y-6 border-r border-gray-800">
-            <NavItem icon="🏠" label="Home" href="/" />
-            <NavItem icon="👩‍💻" label="About" href="/about" />
-            <NavItem icon="📚" label="Skills" href="/skills" />
-            <NavItem icon="📁" label="Projects" href="/projects" />
-            <NavItem icon="✉️" label="Contact" href="/contact" />
-          </aside>
-
-          {/* Main content */}
-          <main className="flex-1 overflow-y-auto p-6 md:p-10">
-            <Providers>{children}</Providers>
-          </main>
-        </div>
-      </body>
+      <body className="bg-black text-white font-sans">{children}</body>
     </html>
-  );
-}
-function NavItem({
-  icon,
-  label,
-  href,
-}: {
-  icon: string;
-  label: string;
-  href: string;
-}) {
-  return (
-    <Link href={href} style={{ textShadow: "0 0 20px  #fff" }}>
-      <div className="flex  items-center text-sm space-x-1 space-y-1 hover:text-[#F4FBA3] cursor-pointer">
-        <span className="text-2xl">{icon}</span>
-        <span>{label}</span>
-      </div>
-    </Link>
   );
 }
